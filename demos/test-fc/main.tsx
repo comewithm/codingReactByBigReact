14befbb44a6969a37dc6fa972e8358bed977aa4f
wanghuan1 <wanghuan1@duiba.com.cn>
2023-08-17 00:02:27 +0800

    feat(useState): 实现useState

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import {useState} from 'react'
 import ReactDOM from 'react-dom/client'
 
 function App() {
+    const [num, setNum] = useState(20)
     return (
         <div>
-            <Child />
+            {num}
         </div>
     )
 }
